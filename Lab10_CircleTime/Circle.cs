2d3b387fe67e1cf3c39e450dbcc248db81176c16
Deno <denobrockert@yahoo.com>
2019-07-25 19:16:38 -0400

    First push of Lab 10

---
+++ Lab10_CircleTime/Circle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_CircleTime
+{
+    public class Circle
+    {
+        #region Field
+        private double radius;
+        private double pi = Math.PI;
+        #endregion
+
+        #region Properties
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+
+        public Circle()
+        {
+
+        }
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+        public double CalculateCircumferce()
+        {
+            double result = (2 * radius) * pi;
+            return Math.Round(result,2);
+        }
+        public double CalculateArea()
+        {
+            double result = (radius * radius) * pi;
+            return Math.Round(result, 2);
+        }
+        public string CalculateFormattedCircumference()
+        {
+            double result = CalculateCircumferce();
+            return result.ToString();
+            
+        }
+        public string CalculateFormattedArea()
+        {
+            double result = CalculateArea();
+            return result.ToString();
+        }
+        public string FormattedNumber(double x)
+        {
+            string formattedNum =  Math.Round(x, 2).ToString();
+            return formattedNum;
+        }
+
+        #endregion
+    }
+}
---
+++ Lab10_CircleTime/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10_CircleTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Welcome to Circle Tester");
+            int num = 0;
+            bool again = true;
+            while (again)
+            {
+                
+                Circle circles = new Circle(TryCatchInput("Please enter a radius length: "));
+
+                Console.WriteLine($"Circumference: {circles.CalculateFormattedCircumference()}");
+                Console.WriteLine($"Area: {circles.CalculateFormattedArea()}");
+                num++;
+
+
+                if(!PlayAgain("Would you like to add another circle?(y/n): "))
+                {
+                    //int numberOfCircles = circles.Count;
+                    Console.WriteLine($"You have {num} circles.\nGood Bye!");
+                    again = false;
+                }
+                
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        public static double ParseInput(string message)
+        {
+            string input = GetUserInput(message);
+            if(double.TryParse(input, out double userDouble))
+            {
+                return userDouble;
+            }
+            else
+            {
+                return ParseInput("Error! That is improper input. Please try again: ");
+            }
+        }
+        public static bool PlayAgain(string message)
+        {
+            string input = "";
+            while (input != "y" && input != "n")
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+            }
+            if (input == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        public static double TryCatchInput(string message)
+        {
+            string input = GetUserInput(message);
+            try
+            {
+                double userNumber = double.Parse(input);
+                return userNumber;
+            }
+            catch (FormatException)
+            {
+
+                return TryCatchInput("That was not a number. Please try again: ");
+            }
+            catch (OverflowException)
+            {
+                return TryCatchInput("That number is too large. Please try again: ");
+
+            }
+            catch
+            {
+                return TryCatchInput("Error! Please try again: ");
+
+            }
+        }
+    }
+}
